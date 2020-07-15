0a6b8b776b1feede77f126a1234ef152c75dab46
Joe Wills <JGW3Code@gmail.com>
2020-07-15 01:24:27 -0500

    should work now

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require("express")
-const morgan = require("morgan")
-const mongoose = require("mongoose")
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(morgan("dev"))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static("public"))
+const app = express();
 
-mongoose.connect("mongodb://localhost/workout", {
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true, 
     useFindAndModify: false,
-})
+});
 
 app.use(require("./routes/api.js"))
 
 app.use(require("./routes/views.js"))
 
-app.listen(3000, ()=>{
+app.listen(PORT, () => {
     console.log ("app is running on port 3000")
-})
+});
 
