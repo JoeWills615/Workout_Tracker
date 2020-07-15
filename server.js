ac24b7089862be4bf813a1c9233fe717d5effb67
Joe Wills <JGW3Code@gmail.com>
2020-07-15 01:39:47 -0500

    on the right track with the tracker

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 app.use(require("./routes/api.js"))
 
-app.use(require("./routes/views.js"))
+
 
 app.listen(PORT, () => {
     console.log ("app is running on port 3000")
